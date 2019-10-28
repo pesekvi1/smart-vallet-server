b99951f203ab49d3e22d233b04bec043d9ecf73e
Viktor Pešek <viktor.pesek12@gmail.com>
2019-10-28 09:00:10 +0100

    adding basic java app with gradle

---
+++ src/test/java/com/smartwallet/main/MainApplicationTests.java
@@ -0,0 +1,12 @@
+package com.smartwallet.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
